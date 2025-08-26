c5762f1470e64c87dbfb14fe3e512bdfc3f286ba
Guilhermechgs <guilhermechgs@gmail.com>
2025-08-26 17:03:20 -0300

    atualização dever de casa

---
+++ Exloop4.js
@@ -0,0 +1,19 @@
+let produtos = [
+    {nome : "refrigerante", preco: 8, vendas : 150 },
+    {nome : "frutas", preco: 5, vendas : 200 },
+    {nome : "doces", preco: 3, vendas : 100 },
+]
+
+function vendatotal(produtos){
+    let receitaTotal = 0;
+    for (const produto of produtos) {
+        receitaTotal += produto.vendas * produto.preco;
+    }
+    return receitaTotal;
+}
+
+const total = vendatotal(produtos);
+produtos.forEach(item => {
+    console.log(`O produto ${item.nome} vendeu ${item.preco} reais por ${item.vendas} unidades`)
+})
+console.log(`A receita total de vendas é: R$${total}`);
