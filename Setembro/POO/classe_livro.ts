5e153b91db21ff54149bb640d0726aa1a4d23460
Guilhermechgs <guilhermechgs@gmail.com>
2025-09-17 13:14:11 -0300

    inicio exercicio classe livro

---
+++ Setembro/POO/classe_livro.ts
@@ -0,0 +1,20 @@
+class livro{
+    nome: string
+    autor: string
+
+    constructor(nome: string, autor: string){
+        this.nome = nome
+        this.autor = autor
+    }
+}
+
+class autor{
+    nome: string
+    idade: number
+
+    constructor(nome: string, idade: number){
+        this.nome = nome
+        this.idade = idade
+    }
+
+}
