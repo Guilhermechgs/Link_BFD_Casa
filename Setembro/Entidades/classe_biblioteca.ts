d7c93797c4cf946f8133150c15fc0ba39782872c
Guilhermechgs <guilhermechgs01@gmail.com>
2025-09-17 20:33:58 -0300

    kill me

---
+++ Setembro/Entidades/classe_biblioteca.ts
@@ -0,0 +1,16 @@
+class Livro{
+    nome : string
+    ano : string
+    autores : string[]
+
+    constructor(nome : string, ano : string){
+        this.nome = nome
+        this.ano = ano
+        this.autores = []
+    }
+}
+
+class Autor{
+    nome : string
+    
+}
---
+++ Setembro/Entidades/teste.ts
@@ -0,0 +1,2 @@
+const nome: string = "TypeScript direto no Node!";
+console.log(nome);
