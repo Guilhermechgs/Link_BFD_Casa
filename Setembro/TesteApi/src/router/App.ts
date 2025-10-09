6d751defe3557e8210571922b03ef9328eee173e
Guilhermechgs <guilhermechgs@gmail.com>
2025-10-09 18:25:12 -0300

    Api pronta

--- Setembro/TesteApi/src/router/App.ts
+++ Setembro/TesteApi/src/router/App.ts
@@ -1,7 +1,10 @@
 import express, {Request, Response} from "express"
+import produtoRouter from "./Produto.router";
 
 const app = express()
 app.use(express.json())
+
+app.use("/api", produtoRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
