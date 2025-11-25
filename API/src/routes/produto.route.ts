2dec731cbdf4f5d0b7e2ea4f2b825a40bff4bbc5
Guilhermechgs <Guilhermechgs01@gmail.com>
2025-11-25 11:09:13 -0300

    falta app

--- API/src/routes/produto.route.ts
+++ API/src/routes/produto.route.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
-import { Produto_Repositorio } from '../repositorio/produto.repositorio.js';
-import { ProdutoController } from '../controller/controller.produto.js';
-import { db } from '../Config/Database.js';
+import { inserirProduto, retornarProduto } from '../controller/controller.produto.js';
+import { Produto } from '../Entidades/produto.js';
 
 const router = Router();
 
-const produtoRepo = new Produto_Repositorio(db);
-const produtoController = new ProdutoController(produtoRepo);
+router.post("/inserirproduto", (req, res) => {
+    const {nome, preco} = req.body
+    const produto = new Produto(nome, preco)
+    res.send("Produto cadastrado com sucesso.")
+})
 
-router.post('/produtos', (req, res) => produtoController.create(req, res));
+router.get("/retornarProduto", retornarProduto)
 
 export default router;
